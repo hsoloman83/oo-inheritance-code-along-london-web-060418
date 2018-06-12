7089617631b022a3742b7712b4e6c25d605efe9d
github username <github email address>
2018-06-12 16:59:03 +0100

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,18 @@
 class Vehicle
 
+  attr_accessor :wheel_size, :wheel_number
+#Instances of Vehicle initialize with a wheel size and number
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+#go and #fill_up_tank instance methods that describe some common vehicle behavior.
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
+
 end
